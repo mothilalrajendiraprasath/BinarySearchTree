9fe4e7a901c660b92f42f41f41ade834c3268ce6
Mothilal Rajendira Prasath <78716398+mothilalrajendiraprasath@users.noreply.github.com>
2021-10-17 14:57:58 +0530

    Adding to the BST

---
+++ BST.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class BST<T extends Comparable<T>>{
+
+    public node newNode(int data){
+        node n = new node();
+        n.data=data;
+        n.left=null;
+        n.right=null;
+        return n;
+    }
+    public node insert(node node, int value) {
+    if(node==null){
+     return newNode(value);
+    }
+
+    if(value<node.data){
+        node.left=insert(node.left,value);   /* recursively calling the function */
+    }
+    else if(value>node.data){
+        node.right=insert(node.right,value);
+    }
+    return node;
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	BST tree=new BST();
+    node root = null;
+    root= tree.insert(root,50);
+    root=tree.insert(root,30);
+    root=tree.insert(root,30);
+    }
+}
